74401b2cd11f056c7d2605ce71d509893e721d32
bishal0602 <bishalpanta60@gmail.com>
2022-06-08 23:40:02 +0545

    added number limit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
   const [previousValue, setPreviousValue] = useState("");
   const [currentValue, setCurrentValue] = useState("");
   const [operator, setOperator] = useState("");
+  const [disableButtons, setDisableButtons] = useState(false);
 
   const reset = () => {
     setCurrentValue("");
@@ -112,13 +113,47 @@ const App = () => {
   function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
+
+  // Number Limiter
+  function numberLimiter() {
+    if (currentValue > 9007199254740991) {
+      setCurrentValue("Number too Big!");
+      setDisableButtons(true);
+      setTimeout(() => {
+        reset();
+        setDisableButtons(false);
+      }, 1500);
+    } else {
+      return false;
+    }
+  }
+
+  // unclickable buttons
+  useEffect(() => {
+    const buttons = document.querySelectorAll(".buttons");
+    const currentVal = document.querySelector(".currentVal");
+    if (disableButtons) {
+      buttons.forEach((button) => {
+        button.setAttribute("id", "disabled-interaction");
+      });
+      currentVal.setAttribute("id", "error");
+    } else {
+      buttons.forEach((button) => {
+        button.removeAttribute("id", "disabled-interaction");
+        currentVal.removeAttribute("id", "error");
+      });
+    }
+  }, [disableButtons]);
+
   return (
-    <div className="font-sans bg-gray-100 h-[80vh] max-w-[90vw] w-[50vh] p-4 rounded-[2rem] sm:shadow-2xl shadow-[0_3px_60px_-12px_rgb(0,0,0,0.3)] text-3xl">
+    <div className="font-sans bg-gray-100 min-h-[500px] h-[80vh] max-w-[90vw] w-[50vh] p-4 rounded-[2rem] sm:shadow-2xl shadow-[0_3px_60px_-12px_rgb(0,0,0,0.3)] text-3xl">
       <div className="h-[100%] w-[100%] grid grid-cols-4 grid-rows-6 gap-2 place-items-center">
         <div className="col-span-full justify-self-end grid grid-rows-3 text-right">
           <div className="text-lime-600 text-2xl font-bold">{operator}</div>
-          <div className="text-3xl">
-            {currentValue === "" ? "" : numberWithCommas(currentValue)}
+          <div className="currentVal text-[1.6rem]">
+            {currentValue === ""
+              ? ""
+              : numberLimiter() || numberWithCommas(currentValue)}
           </div>
           <div className="text-gray-600 text-2xl">{previousValue}</div>
         </div>
@@ -129,96 +164,96 @@ const App = () => {
           C
         </div>
         <div
-          className="grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-lime-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
+          className="buttons grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-lime-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
           onClick={percentage}
         >
           %
         </div>
         <div
-          className="grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-lime-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
+          className="buttons grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-lime-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
           onClick={plusminus}
         >
           +/-
         </div>
         <div
-          className="grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-lime-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
+          className="buttons grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-lime-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
           onClick={operation}
         >
           ÷
         </div>
         <div
-          className="grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-zinc-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
+          className="buttons grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-zinc-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
           onClick={inputDigit}
         >
           7
         </div>
         <div
-          className="grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-zinc-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
+          className="buttons grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-zinc-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
           onClick={inputDigit}
         >
           8
         </div>
         <div
-          className="grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-zinc-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
+          className="buttons grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-zinc-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
           onClick={inputDigit}
         >
           9
         </div>
         <div
-          className="grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-lime-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
+          className="buttons grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-lime-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
           onClick={operation}
         >
           *
         </div>
         <div
-          className="grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-zinc-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
+          className="buttons grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-zinc-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
           onClick={inputDigit}
         >
           4
         </div>
         <div
-          className="grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-zinc-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
+          className="buttons grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-zinc-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
           onClick={inputDigit}
         >
           5
         </div>
         <div
-          className="grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-zinc-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
+          className="buttons grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-zinc-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
           onClick={inputDigit}
         >
           6
         </div>
         <div
-          className="grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-lime-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
+          className="buttons grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-lime-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
           onClick={operation}
         >
           -
         </div>
         <div
-          className="grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-zinc-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
+          className="buttons grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-zinc-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
           onClick={inputDigit}
         >
           1
         </div>
         <div
-          className="grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-zinc-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
+          className="buttons grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-zinc-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
           onClick={inputDigit}
         >
           2
         </div>
         <div
-          className="grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-zinc-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
+          className="buttons grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-zinc-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
           onClick={inputDigit}
         >
           3
         </div>
         <div
-          className="grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-lime-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
+          className="buttons grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-lime-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
           onClick={operation}
         >
           +
         </div>
-        <div className="grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-black rounded-[50%] drop-shadow-md text-2xl hover:bg-lime-700">
+        <div className="buttons grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-black rounded-[50%] drop-shadow-md text-2xl hover:bg-lime-700">
           <a
             href="https://github.com/bishal0602/"
             target="_blank"
@@ -233,19 +268,19 @@ const App = () => {
           </a>
         </div>
         <div
-          className="grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-zinc-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
+          className="buttons grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-zinc-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
           onClick={inputDigit}
         >
           0
         </div>
         <div
-          className="grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-zinc-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
+          className="buttons grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-gray-200 text-zinc-700 rounded-[50%] drop-shadow-md text-2xl hover:bg-gray-300"
           onClick={inputDigit}
         >
           .
         </div>
         <div
-          className="grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-lime-600 text-gray-200 rounded-[50%] drop-shadow-md text-2xl hover:bg-lime-700"
+          className="buttons grid place-content-center sm:h-[10vh] sm:w-[10vh] h-[9vh] w-[9vh] bg-lime-600 text-gray-200 rounded-[50%] drop-shadow-md text-2xl hover:bg-lime-700"
           onClick={equals}
         >
           =
